1c6871cbd9154bbbd689a5aa9ac7fa9371a2c637
Yuekun Li <yuekunli@hotmail.com>
2026-04-09 11:00:29 -0700

    app functional but not deterministic zip

---
+++ FilePacker1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ZipBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxItems = new System.Windows.Forms.ListBox();
+            this.btnAddFiles = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnCreateZip = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxItems
+            // 
+            this.listBoxItems.FormattingEnabled = true;
+            this.listBoxItems.ItemHeight = 16;
+            this.listBoxItems.Location = new System.Drawing.Point(79, 52);
+            this.listBoxItems.Name = "listBoxItems";
+            this.listBoxItems.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxItems.Size = new System.Drawing.Size(448, 164);
+            this.listBoxItems.TabIndex = 0;
+            // 
+            // btnAddFiles
+            // 
+            this.btnAddFiles.Location = new System.Drawing.Point(625, 52);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(130, 44);
+            this.btnAddFiles.TabIndex = 1;
+            this.btnAddFiles.Text = "Add Files";
+            this.btnAddFiles.UseVisualStyleBackColor = true;
+            this.btnAddFiles.Click += new System.EventHandler(this.btnAddFiles_Click);
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(625, 113);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(130, 45);
+            this.btnAddFolder.TabIndex = 2;
+            this.btnAddFolder.Text = "Add Folder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(625, 174);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(130, 42);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove Selected";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnCreateZip
+            // 
+            this.btnCreateZip.Location = new System.Drawing.Point(319, 410);
+            this.btnCreateZip.Name = "btnCreateZip";
+            this.btnCreateZip.Size = new System.Drawing.Size(164, 34);
+            this.btnCreateZip.TabIndex = 4;
+            this.btnCreateZip.Text = "Create Zip";
+            this.btnCreateZip.UseVisualStyleBackColor = true;
+            this.btnCreateZip.Click += new System.EventHandler(this.btnCreateZip_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(79, 303);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(676, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(378, 361);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(48, 16);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 475);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCreateZip);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.btnAddFiles);
+            this.Controls.Add(this.listBoxItems);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Packing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxItems;
+        private System.Windows.Forms.Button btnAddFiles;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnCreateZip;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ FilePacker1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ZipBuilder
+{
+    public partial class Form1 : Form
+    {
+        private List<string> selectedPaths = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var file in dialog.FileNames)
+                    {
+                        selectedPaths.Add(file);
+                        listBoxItems.Items.Add(file);
+                    }
+                }
+            }
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedPaths.Add(dialog.SelectedPath);
+                    listBoxItems.Items.Add(dialog.SelectedPath);
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            var selected = new List<object>();
+
+            foreach (var item in listBoxItems.SelectedItems)
+            {
+                selected.Add(item);
+            }
+
+            foreach (var item in selected)
+            {
+                selectedPaths.Remove(item.ToString());
+                listBoxItems.Items.Remove(item);
+            }
+        }
+
+        private void btnCreateZip_Click(object sender, EventArgs e)
+        {
+            if (selectedPaths.Count == 0)
+            {
+                MessageBox.Show("No files or folders selected.");
+                return;
+            }
+
+            saveFileDialog1.Filter = "Zip files (*.zip)|*.zip";
+            saveFileDialog1.FileName = "archive.zip";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            try
+            {
+                var allFiles = GetAllFiles();
+
+                progressBar1.Value = 0;
+                progressBar1.Maximum = allFiles.Count;
+
+                lblStatus.Text = "Creating zip ...";
+
+                using (ZipArchive archive = ZipFile.Open(saveFileDialog1.FileName, ZipArchiveMode.Create))
+                {
+                    foreach (var fileInfo in allFiles)
+                    {
+                        archive.CreateEntryFromFile(fileInfo.FullPath, fileInfo.ZipPath, CompressionLevel.Optimal);
+
+                        progressBar1.Value++;
+
+                        lblStatus.Text = $"Processing {progressBar1.Value} / {progressBar1.Maximum}";
+                        Application.DoEvents(); // keep UI responsive
+                    }
+                }
+                lblStatus.Text = "Done";
+                MessageBox.Show("Zip file created successfully!", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void AddFileToZip(ZipArchive archive, string filePath, string entryName)
+        {
+            archive.CreateEntryFromFile(filePath, entryName, CompressionLevel.Optimal);
+        }
+
+        private void AddDirectoryToZip(ZipArchive archive, string folderPath, string entryName)
+        {
+            foreach (var file in Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories))
+            {
+                var relativePath = GetRelativePath(folderPath, file);
+                var zipPath = Path.Combine(entryName, relativePath);
+
+                archive.CreateEntryFromFile(file, zipPath, CompressionLevel.Optimal);
+            }
+        }
+
+        private string GetRelativePath(string baseDir, string fullPath)
+        {
+            Uri baseUri = new Uri(baseDir.EndsWith("\\") ? baseDir : baseDir + "\\");
+            Uri fullUri = new Uri(fullPath);
+            return Uri.UnescapeDataString(
+                baseUri.MakeRelativeUri(fullUri)
+                .ToString()
+                .Replace('/', '\\'));
+        }
+
+        private class FileEntry
+        {
+            public string FullPath { get; set; }
+            public string ZipPath { get; set; }
+        }
+
+        private List<FileEntry> GetAllFiles()
+        {
+            var result = new List<FileEntry>();
+
+            foreach (var path in selectedPaths)
+            {
+                if (File.Exists(path))
+                {
+                    result.Add(new FileEntry
+                    {
+                        FullPath = path,
+                        ZipPath = Path.GetFileName(path)
+                    });
+                }
+                else if (Directory.Exists(path))
+                {
+                    var baseName = Path.GetFileName(path);
+
+                    foreach (var file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+                    {
+                        var relative = GetRelativePath(path, file);
+                        result.Add(new FileEntry
+                        {
+                            FullPath = file,
+                            ZipPath = Path.Combine(baseName, relative)
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
