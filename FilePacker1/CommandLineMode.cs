7456f8ada71d4f848d21ade2bd7f837183e48aba
Yuekun Li <yuekunli@hotmail.com>
2026-05-01 17:16:45 -0700

    parse command line arg

--- FilePacker1/CommandLineMode.cs
+++ FilePacker1/CommandLineMode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -12,24 +13,80 @@ namespace ZipBuilder
     {
         public void Run(string[] args)
         {
+            
+            //if (args.Length < 3)
+            //{
+            //    ShowUsage();
+            //    return;
+            //}
+
+            //var inputs = new List<string>();
+
+            //string selectedOS = args[0];
+            //string outputPath = args[1];
+            //for (int i = 2; i < args.Length; ++i)
+            //{
+            //    inputs.Add(args[i]);
+            //}
+
+
+            bool isQuiet = false;
+            bool isVerbose = false;
+            string targetOS = null;
+            string outputPath = null;
+            List<string> inputFiles = new List<string>();
+            bool isShowHelp = false;
+
+            var p = new OptionSet()
+            {
+                { "q|quiet", "Suppress messages", v => isQuiet = v != null },
+                { "v|verbose", "Detailed progress message", v => isVerbose = v != null },
+                { "t|target=", "Target OS (Windows or Linux/macOS)", v => targetOS = v },
+                { "o|output=", "Path to output file", v => outputPath = v },
+                { "h|help", "Show help", v => isShowHelp = v != null }
+            };
+
             try
             {
-                if (args.Length < 3)
+                inputFiles = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("Command Parsing Error: ");
+                Console.Write(e.Message);
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (isShowHelp)
                 {
-                    ShowUsage();
+                    ShowHelp();
                     return;
                 }
 
-                var inputs = new List<string>();
-
-                string selectedOS = args[0];
-                string outputPath = args[1];
-                for (int i = 2; i < args.Length; ++i)
+                if (outputPath == null)
                 {
-                    inputs.Add(args[i]);
+                    Console.WriteLine("output path is missing");
+                    return;
                 }
 
-                CreateDeterministicZip(selectedOS, outputPath, inputs);
+                if (inputFiles.Count == 0)
+                {
+                    Console.WriteLine("input file(s) not provided");
+                    return;
+                }
+
+                if (targetOS == null)
+                {
+                    Console.WriteLine("target OS is missing");
+                    return;
+                }
+
+                CreateDeterministicZip(targetOS, isVerbose, outputPath, inputFiles);
 
                 Console.WriteLine("Zip created successfully!");
             }
@@ -39,6 +96,27 @@ namespace ZipBuilder
             }
         }
 
+        private bool shouldNormalizePath(string selectedOS)
+        {
+            // string.Equals(selectedOS, "windows", StringComparison.OrdinalIgnoreCase)
+
+            if ("windows".StartsWith(selectedOS, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            
+            if ("linux".StartsWith(selectedOS, StringComparison.OrdinalIgnoreCase)) 
+            { 
+                return false; 
+            }
+            
+            if ("macOS".StartsWith(selectedOS, StringComparison.OrdinalIgnoreCase)) 
+            { 
+                return false; 
+            }
+            
+            throw new Exception("invalid target operating system: " + selectedOS);
+        }
 
         private string GetRelativePath(string baseDir, string fullPath)
         {
@@ -65,7 +143,7 @@ namespace ZipBuilder
                 if (File.Exists(path))
                 {
                     string zipPath = Path.GetFileName(path);
-                    if (selectedOS == "Windows")
+                    if (shouldNormalizePath(selectedOS))
                     {
                         zipPath = zipPath.ToLowerInvariant();
                     }
@@ -83,7 +161,7 @@ namespace ZipBuilder
                     {
                         var relative = GetRelativePath(path, file);
                         string zipPath = Path.Combine(baseName, relative);
-                        if (selectedOS == "Windows")
+                        if (shouldNormalizePath(selectedOS))
                         {
                             zipPath = zipPath.ToLowerInvariant();
                         }
@@ -98,20 +176,23 @@ namespace ZipBuilder
             return result;
         }
 
-        private void CreateDeterministicZip(string selectedOS, string zipPath, List<string> inputs)
+        private void CreateDeterministicZip(string selectedOS, bool isVerbose, string zipPath, List<string> inputs)
         {
             var allFiles = GetAllFiles(inputs, selectedOS);
 
             var sorted = allFiles.OrderBy(f => f.ZipPath, StringComparer.Ordinal).ToList();
+            // f.ZipPath is this file's relative path inside the zip
 
-            using (var fs = new FileStream(zipPath, FileMode.Create))
-            using (var archive = new ZipArchive(fs, ZipArchiveMode.Create))
+            using (var outputFs = new FileStream(zipPath, FileMode.Create))
+            using (var archive = new ZipArchive(outputFs, ZipArchiveMode.Create))
             {
                 foreach (var file in sorted)
                 {
+                    if (isVerbose) Console.WriteLine("Processing: " + file.FullPath);
                     AddDeterministicEntry(archive, file.FullPath, file.ZipPath);
                 }
             }
+            ZipEnforcer.SetGeneralPurposeBit(zipPath);
         }
 
         private void AddDeterministicEntry(ZipArchive archive, string filePath, string entryName)
@@ -135,10 +216,20 @@ namespace ZipBuilder
             }
         }
 
-        private static void ShowUsage()
+        private static void ShowHelp()
         {
+            Console.WriteLine();
             Console.WriteLine("Usage:");
-            Console.WriteLine("filepacker.exe Windows|Linux output.zip input1 input2 ...");
+            Console.WriteLine();
+            Console.WriteLine("contentpacker.exe [-verbose] -target [windows|linux|macos] -output output.zip  -input  input1  input2 ...");
+            Console.WriteLine();
+            Console.WriteLine("single letter options:");
+            Console.WriteLine();
+            Console.WriteLine("contentpacker.exe [-v] -t [w|l|m] -o output.zip -i input1 input2 input3");
+            Console.WriteLine();
+            Console.WriteLine("Either using or not using quotes around path is fine:");
+            Console.WriteLine();
+            Console.WriteLine("contetnpacker.exe -v -t windows -o \"C:\\fully qualified\\path to\\output\" -input input1 \"C:\\fully qualifed\\path to\\input2\"");
         }
     }
 }
